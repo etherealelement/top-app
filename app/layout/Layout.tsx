3db0b1dd397c16be007f4ddabb66785783631af8
ethereal element <vitalik.safonov@list.ru>
2023-05-22 18:00:14 +0300

    createa: footer blank

--- app/layout/Layout.tsx
+++ app/layout/Layout.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { LayoutProps } from "./Layout.props";
+import { Header } from "./Header/Header";
+import { Sidebar } from "./Sidebar/Sidebar";
+import { Footer } from "./Footer/Footer";
 
 
 export const Layout = ({children}: LayoutProps):JSX.Element => {
