afef902c4dd6dfa32e923c2abc7f8258a47ec266
ethereal element <vitalik.safonov@list.ru>
2023-05-23 21:01:29 +0300

    create: axios response

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@
 import React, { useEffect, useState } from "react";
 import { Button, Htag, Ptag, Raiting, Tag } from "./components";
 import { withLayout } from "./layout/Layout";
+import { GetStaticProps } from "next";
+import axios from "axios";
+import { MenuItem } from "./interfaces/menu.interface";
 
-function Home(): JSX.Element {
+function Home({ menu, firstCategory }: HomeProps): JSX.Element {
 	const [rating, setRating] = useState<number>(4);
 
 	return (
@@ -29,8 +32,31 @@ function Home(): JSX.Element {
 				isEditable
 				setRaiting={setRating}
 			></Raiting>
+			<ul>		
+				{menu.map((m) => (
+					<li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+				))}
+			</ul>
 		</>
 	);
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+	const firstCategory = 0;
+	const { data: menu } = await axios.post("https://courses-top.ru/api/top-page/find", {
+		firstCategory
+	});
+	return {
+		props: {
+			menu,
+			firstCategory,
+		},
+	};
+};
+
+interface HomeProps extends Record<string, unknown> {
+	menu: MenuItem[];
+	firstCategory: number;
+}
